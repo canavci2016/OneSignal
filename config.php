e955f53c035e7f1e0b8a5eb28ac0c0ddefb4a180
canavci2016 <canavci2016@gmail.com>
2016-10-21 17:38:39 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'appId' => '6d7cc380-352a-4d26-9b04-31a7a03d14f2',
-    'restApiKey' => 'ZWNmMzIyYjEtOTA1MS00YWY0LWE1MTMtNWYyNTFkMDQ0ZWU1',
-    'authToken' => 'MGMyZjk4NWItMTlhMC00MzEwLWI2MGItZjNiMjA4MTViMWQz',
+    'appId' => 'app id',
+    'restApiKey' => 'key in app',
+    'authToken' => 'token your panel',
 ];
